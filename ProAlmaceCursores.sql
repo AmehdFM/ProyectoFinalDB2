d2df2ac40b43f2da87f4423113c5d3c08f6d736a
FernandoZuniga97 <157933928+FernandoZuniga97@users.noreply.github.com>
2026-04-09 23:28:25 -0600

    Add files via upload
    ya con cursores

--- ProAlmaceCursores.sql
+++ ProAlmaceCursores.sql
@@ -1,4 +1,4 @@
--- Procedimiento almacenado para registrar una venta con manejo transaccional
+--spRegistrarVenta
 create or alter procedure spRegistrarVenta
     @clienteid int, 
     @loteid int, 
@@ -10,7 +10,11 @@ create or alter procedure spRegistrarVenta
 as
 begin
     begin transaction
-    declare @ventaID int, @err int = 0
+    declare @ventaID int, @err int = 0, @etapaID int, @cuotaNumero int, @montoCuota float
+    declare @fechaVencimiento datetime, @capital float, @interesCuota float
+
+    -- Obtener el EtapaID desde la tabla Bloque
+    select @etapaID = EtapaID from Bloque where BloqueID = (select BloqueID from Lote where LoteID = @loteid)
 
     -- Insertar venta
     insert into Venta (LoteID, ClienteID, EmpleadoID, Tipo, Prima, Plazo, Interes)
@@ -26,13 +30,24 @@ begin
     -- Obtener el ID de la venta insertada
     select @ventaID = scope_identity()
 
-    -- Insertar plan de pago (con cuotas iniciales)
-    declare @cuotaNumero int = 1
-    declare @montoCuota float = (@prima + (select Area from Lote where LoteID = @loteid) * @interes) / @plazo
-    while @cuotaNumero <= @plazo
+    -- Inicializar cursor para recorrer las cuotas
+    declare cuota_cursor cursor for
+    select top(@plazo) row_number() over (order by (select null)) as CuotaNumero
+    from master.dbo.spt_values -- Dummy table for row numbers (replace with a suitable table)
+    
+    open cuota_cursor
+    fetch next from cuota_cursor into @cuotaNumero
+
+    while @@FETCH_STATUS = 0
     begin
+        set @montoCuota = (@prima + (select Area from Lote where LoteID = @loteid) * @interes) / @plazo
+        set @capital = @montoCuota * 0.8
+        set @interesCuota = @montoCuota * 0.2
+        set @fechaVencimiento = dateadd(month, @cuotaNumero, getdate())
+
+        -- Insertar plan de pago para la cuota
         insert into PlanPago (VentaID, CuotaNumero, FechaVencimiento, Monto, Capital, Interes)
-        values (@ventaID, @cuotaNumero, dateadd(month, @cuotaNumero, getdate()), @montoCuota, @montoCuota * 0.8, @montoCuota * 0.2)
+        values (@ventaID, @cuotaNumero, @fechaVencimiento, @montoCuota, @capital, @interesCuota)
 
         if @@ERROR != 0 
         begin
@@ -41,8 +56,11 @@ begin
             return
         end
 
-        set @cuotaNumero = @cuotaNumero + 1
+        fetch next from cuota_cursor into @cuotaNumero
     end
+
+    close cuota_cursor
+    deallocate cuota_cursor
 
     -- Actualizar el estado del lote
     update Lote set Estado = 'Vendido' where LoteID = @loteid
@@ -72,11 +90,12 @@ create or alter procedure spRegistrarPago
 as
 begin
     begin transaction
-    declare @pagoID int, @planPagoID int, @err int = 0
+    declare @pagoID int, @planPagoID int, @err int = 0, @estadoPago varchar(20)
 
     -- Obtener el Plan de Pago correspondiente
-    select @planPagoID = PlanPagoID from PlanPago 
-    where VentaID = @ventaID and CuotaNumero = @cuotaNumero
+    select @planPagoID = pp.PlanPagoID from PlanPago pp
+    join Venta v on pp.VentaID = v.VentaID
+    where v.VentaID = @ventaID and pp.CuotaNumero = @cuotaNumero
 
     if @@ERROR != 0 or @planPagoID is null
     begin
@@ -96,15 +115,24 @@ begin
         return
     end
 
-    -- Actualizar el estado de la cuota
-    update PlanPago set Estado = 'Pagado' where PlanPagoID = @planPagoID
+    -- Usamos cursor para actualizar los pagos y estado
+    declare pago_cursor cursor for
+    select Estado from PlanPago where PlanPagoID = @planPagoID
+    
+    open pago_cursor
+    fetch next from pago_cursor into @estadoPago
 
-    if @@ERROR != 0
+    while @@FETCH_STATUS = 0
     begin
-        set @err = -1
-        rollback
-        return
+        if @estadoPago != 'Pagado'
+        begin
+            update PlanPago set Estado = 'Pagado' where PlanPagoID = @planPagoID
+        end
+        fetch next from pago_cursor into @estadoPago
     end
+
+    close pago_cursor
+    deallocate pago_cursor
 
     -- Commit si todo fue exitoso
     if @err = 0
@@ -135,15 +163,21 @@ begin
         return
     end
 
-    -- Actualizar el estado financiero del proyecto (si es necesario)
-    -- Este paso depende de los requisitos adicionales, si existen
+    -- Usar un cursor para actualizar el estado del gasto en el proyecto
+    declare gasto_cursor cursor for
+    select ProyectoID from Gastos where GastoID = scope_identity()
+    
+    open gasto_cursor
+    fetch next from gasto_cursor into @proyectoID
 
-    if @@ERROR != 0
+    while @@FETCH_STATUS = 0
     begin
-        set @err = -1
-        rollback
-        return
+        -- Realizar cualquier operación adicional con el gasto registrado
+        fetch next from gasto_cursor into @proyectoID
     end
+
+    close gasto_cursor
+    deallocate gasto_cursor
 
     -- Commit si todo fue exitoso
     if @err = 0
